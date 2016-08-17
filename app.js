7236510cf7adc93e096c64358cd543a1285a28c5
Tais <tais@Annas-MacBook-Air.local>
2016-08-17 17:19:58 +0300

    вывод текущей даты и времени

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 "use strict";
-document.getElementsByClassName("hello")[0].innerHTML = 'Hello World';
+
+window.onload = function () {
+    var 
+ 		  containerDate = document.getElementsByClassName("container-date"),
+ 		  containerTime = document.getElementsByClassName("container-time"),
+	    date = new Date();
+
+  if (containerDate.length == 0 || containerTime.length == 0) return;
+	
+  containerDate[0].innerHTML = `Today is : ${date.toLocaleString("en-US", { weekday: 'long' })}.`;
+
+	containerTime[0].innerHTML =  `Current time is : ${date.toLocaleString("en-US", { hour: 'numeric' })} : 
+  ${date.getMinutes()} : ${date.getSeconds()}`;
+
+}
