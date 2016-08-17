538a120e34232ce126b66e7320c9f04762bc4e80
Tais <tais@Annas-MacBook-Air.local>
2016-08-17 18:27:44 +0300

    compete Date tutorial

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 "use strict";
 
+var 
+  date = new Date(),
+  containerDate,
+  containerTime,
+  intervalID;
+
 window.onload = function () {
-    var 
- 		  containerDate = document.getElementsByClassName("container-date"),
- 		  containerTime = document.getElementsByClassName("container-time"),
-	    date = new Date();
+    
+  containerDate = document.getElementsByClassName("container-date");
+  containerTime = document.getElementsByClassName("container-time");
 
   if (containerDate.length == 0 || containerTime.length == 0) return;
 	
   containerDate[0].innerHTML = `Today is : ${date.toLocaleString("en-US", { weekday: 'long' })}.`;
+  
+  intervalID = window.setInterval(secondsTimer, 1000);
 
-	containerTime[0].innerHTML =  `Current time is : ${date.toLocaleString("en-US", { hour: 'numeric' })} : 
-  ${date.getMinutes()} : ${date.getSeconds()}`;
-
+  secondsTimer();
 }
+
+function secondsTimer() {
+  date = new Date();
+  containerTime[0].innerHTML =  `Current time is : ${date.toLocaleString("en-US", { hour: 'numeric' })} : 
+  ${date.getMinutes()} : ${date.getSeconds()}`;
+} 
