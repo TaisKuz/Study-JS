3fd7fa87025e65e2fba3ed4d8816a0653a3f9e84
Tais <tais@Annas-MacBook-Air.local>
2016-09-09 17:28:43 +0300

    bad jump.

--- ball/js/ball.js
+++ ball/js/ball.js
@@ -1,19 +1,95 @@
 "use strict";
 
-var ball = document.getElementById("ball");
 
-var listener = function(e) {
-  ball.style.left = e.pageX;
-  ball.style.top = e.pageY;
+window.onload = function() {
+var ball = document.getElementById('ball');
+
+ball.onmousedown = function(e) {
+
+  var coords = getCoords(ball);
+  var shiftX = e.pageX - coords.left;
+  var shiftY = e.pageY - coords.top;
+
+  ball.style.position = 'absolute';
+  document.body.appendChild(ball);
+  moveAt(e);
+
+  ball.style.zIndex = 1000; // над другими элементами
+
+  function moveAt(e) {
+    ball.style.left = e.pageX - shiftX + 'px';
+    ball.style.top = e.pageY - shiftY + 'px';
+  }
+
+  document.onmousemove = function(e) {
+    moveAt(e);
+  };
+
+  (function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+  ball.onmouseup = function() {
+    document.onmousemove = null;
+    //ball.onmouseup = null;
+    drop();
+	//window.setInterval(drop, 500);
+
+  };
+
+  function getCoords(elem) { // кроме IE8-
+  	var box = elem.getBoundingClientRect();
+
+  	return {
+   	 top: box.top + pageYOffset,
+   	 left: box.left + pageXOffset
+  	};
+  }
+
+  function drop() {
+    var height = (450 - e.pageY);
+    var height2 = (e.pageY);
+
+    function down(){
+      ball.style.top = (450 - 140) + 'px';
+      console.log("down "+ball.style.top );
+
+    }
+    function up(){
+
+      while (e.pageY + height2 > 300)
+      {
+        height2 *= 0.5;
+        ball.style.top = (e.pageY + height2) + 'px';
+            console.log("up "+ball.style.top );
+          }
+
+   }
+ 
+     window.setInterval(down, 50);
+   window.setInterval(up, 500);
+
+  }
+
+
+
+
+}
+
+ball.ondragstart = function() {
+  return false;
 };
 
-function move() {
-  document.addEventListener('mousemove', listener);
+
+
 }
 
-function stop() {
-  document.removeEventListener('mousemove', listener);
-}
 
-ball.onmousedown = move; 
-ball.onmouseup = stop;
+
+
+
+
+
+
